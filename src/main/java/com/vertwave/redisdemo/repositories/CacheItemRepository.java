71140fc8069bfd1f32a529c33d1da98a8710aaff
jonlk <jlkovach01@gmail.com>
2021-12-16 00:03:54 -0500

    some updates

--- src/main/java/com/vertwave/redisdemo/repositories/CacheItemRepository.java
+++ src/main/java/com/vertwave/redisdemo/repositories/CacheItemRepository.java
@@ -4,8 +4,6 @@ import com.vertwave.redisdemo.domain.model.CacheItem;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface CacheItemRepository extends CrudRepository<CacheItem, String> {
 }
