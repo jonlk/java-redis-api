4ee6a84116578554d517842af60d2d3c037fe525
jonlk <jlkovach01@gmail.com>
2021-12-21 14:34:12 -0500

    cache controller updates

--- src/main/java/com/vertwave/jredisapi/controllers/CacheController.java
+++ src/main/java/com/vertwave/jredisapi/controllers/CacheController.java
@@ -3,7 +3,6 @@ package com.vertwave.jredisapi.controllers;
 import com.vertwave.jredisapi.domain.model.CacheItem;
 import com.vertwave.jredisapi.services.CacheService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,28 +25,29 @@ public class CacheController {
     public ResponseEntity<CacheItem> setCacheItem(@RequestBody CacheItem item) {
         return this.cacheService
                 .setCacheItem(item)
-                .map(x -> new ResponseEntity<>(x, HttpStatus.OK))
-                .orElse(new ResponseEntity<>(null, HttpStatus.BAD_REQUEST));
+                .map(x -> ResponseEntity.ok().body(x))
+                .orElse(ResponseEntity.badRequest().build());
     }
 
     @GetMapping("get/{id}")
     public ResponseEntity<CacheItem> getCacheItem(@PathVariable String id) {
         return this.cacheService
                 .getCacheItem(id)
-                .map(x -> new ResponseEntity<>(x, HttpStatus.OK))
-                .orElse(new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+                .map(x -> ResponseEntity.ok().body(x))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("all")
     public ResponseEntity<List<CacheItem>> getAllCacheItems() {
-        return new ResponseEntity<>(this.cacheService
-                .getAllCacheItems(), HttpStatus.OK);
-
+        return ResponseEntity
+                .ok()
+                .body(this.cacheService
+                        .getAllCacheItems());
     }
 
     @DeleteMapping("delete/{id}")
     public ResponseEntity<String> deleteCacheItem(@PathVariable String id) {
         this.cacheService.deleteCacheItem(id);
-        return new ResponseEntity<>("OK", HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 }
